56f26fa439a8e943b3a2e069eed794a93defa9f9
José Eduardo Almeida <web2solucoes@gmail.com>
2024-06-08 15:55:20 -0300

    chore(BaseService):  type improvements

--- src/domains/ports/BaseService.ts
+++ src/domains/ports/BaseService.ts
@@ -1,19 +1,40 @@
 import { IServiceResponse } from '@src/domains/ports/IServiceResponse';
-import { IServiceConfig, TRepos } from './IServiceConfig';
+import { IServiceConfig, TRepos, TServices } from './IServiceConfig';
 import { IPagingRequest } from './persistence/IPagingRequest';
+import { BaseRepo } from './persistence/BaseRepo';
+import { BaseModel } from './persistence/BaseModel';
 
-export abstract class BaseService<ResponseDataEntity, RequestCreate, RequestUpdate> {
+export abstract class BaseService<ResponseDataEntity, RequestCreateDTO, RequestUpdateDTO> {
+  /**
+   * repo is the main data repo associated to the service.
+   * Mandatory
+   */
+  public repo: BaseRepo<BaseModel<Record<any, any>>, Record<any, any>, Record<any, any>>;
+
+  /**
+   * repos hold a collection of injected repos to be used when needed inside the service
+   * Not Mandatory
+   */
   public repos: TRepos;
+
+  /**
+   * services hold a collection of injected services to be used when needed inside the service
+   * Not Mandatory
+   */
+  public services: TServices;
 
   constructor(config: IServiceConfig) {
     this.repos = config.repos ?? {};
+    this.services = config.repos ?? {};
+    if (!config.repo) throw Error('You must provide a data repository when creating a service instance.');
+    this.repo = config.repo;
   }
 
-  public abstract create(data: RequestCreate): Promise<IServiceResponse<ResponseDataEntity>>;
+  public abstract create(data: RequestCreateDTO): Promise<IServiceResponse<ResponseDataEntity>>;
 
   public abstract update(
     id: string,
-    data: RequestUpdate
+    data: RequestUpdateDTO
   ): Promise<IServiceResponse<ResponseDataEntity>>;
 
   public abstract delete(id: string): Promise<IServiceResponse<boolean>>;
--- src/domains/ports/IServiceConfig.ts
+++ src/domains/ports/IServiceConfig.ts
@@ -1,14 +1,12 @@
-import { IAccount, Account } from '@src/domains/Accounts';
-import { ITransaction, Transaction } from '@src/domains/Transactions';
-import { IUser, User } from '@src/domains/Users';
-import { BaseRepo } from './BaseRepo';
-import { BaseService } from './BaseService';
+import { BaseModel } from './persistence/BaseModel';
+import { BaseRepo } from './persistence/BaseRepo';
+
 // import { IStore } from './IStore';
-export type TRepos = Record<string, BaseRepo<IAccount | ITransaction | IUser>>
-export type TServices =
-  Record<string, BaseService<IAccount | ITransaction | IUser, Account | Transaction | User>>
+export type TRepos = Record<any, any>;
+export type TServices = Record<any, any>
 
 export interface IServiceConfig {
   repos?: TRepos;
   services?: TServices;
+  repo: BaseRepo<BaseModel<Record<any, any>>, Record<any, any>, Record<any, any>>
 }
