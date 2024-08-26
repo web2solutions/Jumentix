d817bfcbcb7320e1ab94e61838375462f34962c0
Eduardo Almeida <web2solucoes@gmail.com>
2024-08-25 21:09:00 -0300

    build(package.json): run lint-fix in pretest. Add jsonwebtoken
    Run lint-fix in pretest. Add jsonwebtoken as dependency to suppot the Bearer HTTP AUTH
    
    BREAKING CHANGE: Whener the tests are runned they will be preceded by eslint run. That will require
    a lower lever coding cleaning befor event running tests

--- src/infra/server/HTTP/adapters/fastify/FastifyServer.ts
+++ src/infra/server/HTTP/adapters/fastify/FastifyServer.ts
@@ -76,7 +76,7 @@ class FastifyServer extends HTTPBaseServer<Fastify> {
   // eslint-disable-next-line class-methods-use-this
   public async stop(code: number = 0) {
     this._application.close();
-    process.exit(code);
+    // process.exit(code);
   }
 }
 
