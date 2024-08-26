4f59582d3061c7f65d65c426d8243c7b30330d0d
Eduardo Almeida <web2solucoes@gmail.com>
2024-08-26 01:19:57 -0300

    chore: delete old files

--- src/infra/server/HTTP/validators/index.ts
+++ src/infra/server/HTTP/validators/index.ts
@@ -1,4 +1,3 @@
-export { default as isUserAccessGranted } from './isUserAccessGranted';
 export { default as throwIfOASInputValidationFails } from './throwIfOASInputValidationFails';
 export { default as isPropertiesMatching } from './isPropertiesMatching';
 export { default as getSchema } from './getSchema';
